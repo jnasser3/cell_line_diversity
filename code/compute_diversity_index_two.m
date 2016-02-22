2589ad65ba65e916413ee23c155b788059c0275d
Joseph Nasser <jnasser@broadinstitute.org>
2016-02-22 18:56:32 -0500

    various updates

--- code/compute_diversity_index_two.m
+++ code/compute_diversity_index_two.m
@@ -1,16 +1,16 @@
 function DI = compute_diversity_index_two(ds1, ds2, varargin)
 %Given expression matrices for two cell lines, computes their diversity index.
 
-pnames = {'corr_type',...
+pnames = {'metric',...
     'make_plot',...
     'show_plot',...
     'save_plot',...
     'plot_dir'};
-dflts = {'pearson',...
+dflts = {'basic_bioa_corr',...
     false,...
     false,...
     false,...
-    '/cmap/projects/cell_line_diversity/analysis/pairwise_diversity_index/fig'};
+    ''};
 args = parse_args(pnames, dflts, varargin{:});
 
 % Data validation
@@ -19,56 +19,14 @@ assert(isequal(ds1.rid,ds2.rid), 'Gene space not the same')
 % Order the perts identically b/w ds1 and ds2
 [ds1,ds2] = order_data_sets(ds1, ds2);
 
-%compute bioactivity weights
-max_ccq_75 = max(...
-        [cell2mat(ds1.cdesc(:,ds1.cdict('distil_cc_q75')))
-        cell2mat(ds2.cdesc(:,ds1.cdict('distil_cc_q75')))]...
-        );
-bioactivity_mat = [...
-    cell2mat(ds1.cdesc(:,ds1.cdict('distil_cc_q75'))),...
-    cell2mat(ds2.cdesc(:,ds2.cdict('distil_cc_q75'))),...
-    zeros(size(ds1.mat,2),1)
-    ];
-%bioa = max(bioactivity_mat,[],2)/max_ccq_75;
-bioa = max(bioactivity_mat,[],2);
-
-%compute pairwise correlations
-corrs = pwcorr(ds1.mat,ds2.mat);
-
-%compute diversity index
-num_perts = size(ds1.mat,2);
-DI = sum(bioa .* (1 - corrs)) / num_perts;
-
-%old calculation
-% for ii = 1:num_perts
-%     bioa(ii) = max([cell2mat(ds1.cdesc(ii,ds1.cdict('distil_cc_q75')))
-%         cell2mat(ds2.cdesc(ii,ds1.cdict('distil_cc_q75')))
-%         0])/max_ccq_75;
-%     corrs(ii) = corr(ds1.mat(:,ii),ds2.mat(:,ii),...
-%                     'type',args.corr_type);
-%     DI = DI + bioa(ii)*(1 - corrs(ii));
-% end
-
-if args.make_plot
-    if args.show_plot
-        figure;
-    else
-        figure('Visible','Off');
-    end
-    namefig(strcat(ds1.cdesc{1,ds1.cdict('cell_id')},'_',ds2.cdesc{1,ds2.cdict('cell_id')}));
-    scatter(bioa,corrs,[],bioa .* (1 - corrs))
-    axis([0 1 0 1])
-    colormap(cool)
-    colorbar
-    caxis([0, 1])
-    xlabel('Max Bioactivity')
-    ylabel('Correlation')
-    title_str = sprintf('Bioactivity vs correlation for perts in %s and %s,\n Num perts is: %d \n Diversity index is: %.3f',...
-        ds1.cdesc{1,ds1.cdict('cell_id')},...
-        ds2.cdesc{1,ds2.cdict('cell_id')},...
-        num_perts,...
-        DI);
-    title(title_str)
+switch lower(args.metric)
+    case 'basic_bioa_corr'
+        DI = basic_bioa_corr(ds1,ds2,args);
+    case 'rel_wtd_corr'
+        DI = rel_wtd_corr(ds1,ds2,args);
+    case 'cov_fro'
+        DI = covariance_frobenius(ds1,ds2);  
+    case 'cov_eig'
 end
 
 end
@@ -107,5 +65,81 @@ for ii = 1:numel(dups)
 end
 
 out_ds = ds_slice(ds,'cid',ds.cid(idx_to_remove),'exclude_cid',true);
+
+end
+
+function DI = basic_bioa_corr(ds1,ds2,args)
+%compute diversity index using a basic sum over max(bioa)*(1 - corr)
+%metric.
+
+%compute bioactivity weights
+bioactivity_mat = [...
+    cell2mat(ds1.cdesc(:,ds1.cdict('distil_cc_q75'))),...
+    cell2mat(ds2.cdesc(:,ds2.cdict('distil_cc_q75'))),...
+    zeros(size(ds1.mat,2),1)
+    ];
+bioa = max(bioactivity_mat,[],2);
+
+%compute pairwise correlations
+pwcorrs = pwcorr(ds1.mat,ds2.mat);
+
+%compute diversity index
+num_perts = size(ds1.mat,2);
+DI = sum(bioa .* (1 - pwcorrs)) / num_perts;
+
+if args.make_plot
+    if args.show_plot
+        h = figure;
+    else
+        h = figure('Visible','Off');
+    end
+    name = strcat(ds1.cdesc{1,ds1.cdict('cell_id')},...
+        '_',ds2.cdesc{1,ds2.cdict('cell_id')});
+    namefig(h,name);
+    scatter(bioa,pwcorrs,[],bioa .* (1 - pwcorrs))
+    axis([0 1 -1 1])
+    colormap(cool)
+    colorbar
+    caxis([0, 1])
+    grid on
+    xlabel('Max Bioactivity')
+    ylabel('Correlation')
+    title_str = sprintf('Bioactivity vs correlation for perts in %s and %s,\n Num perts is: %d \n Diversity index is: %.3f',...
+        ds1.cdesc{1,ds1.cdict('cell_id')},...
+        ds2.cdesc{1,ds2.cdict('cell_id')},...
+        num_perts,...
+        DI);
+    title(title_str)
+    
+    if args.save_plot
+        saveas(h,strcat(fullfile(args.plot_dir,name),'.png'),'png')
+        close;
+    end
+end
+
+end
+
+function DI = covariance_frobenius(ds1,ds2)
+% Compute the frobenius norm of the difference in the covariance matrices.
+
+cov1 = cov(ds1.mat');
+cov2 = cov(ds2.mat');
+DI = norm(cov1 - cov2, 'fro');
+end
+
+function DI = rel_wtd_corr(ds1,ds2,args)
+
+%get corr contributions
+all_corrs = fastcorr(ds1.mat,ds2.mat);
+corr_contributions = xform_corr_diversity(diag(all_corrs),tri2vec(all_corrs));
+
+%get bioactiviy cdfs and convert to cdf contributions
+bioa1 = cell2mat(ds1.cdesc(:,ds1.cdict('distil_cc_q75')));
+bioa1 = rankorder(bioa1)/numel(bioa1);
+bioa2 = cell2mat(ds1.cdesc(:,ds1.cdict('distil_cc_q75')));
+bioa2 = rankorder(bioa2)/numel(bioa2);
+
+max_bioa = max([bioa1, bioa2], [], 2);
+DI = sum(corr_contributions .* max_bioa) / sum(max_bioa);
 
 end
--- code/compute_pairwise_diversity_index.m
+++ code/compute_pairwise_diversity_index.m
@@ -1,38 +1,47 @@
 function pdi = compute_pairwise_diversity_index(ds, varargin)
+%pdi = compute_pairwise_diversity_index(ds, varargin)
+%
 %Given a ds of signatures, computes the pairwise diversity index for all
 %cell lines
 %
 %Input: ds: a genes x perturbagens struct containing the gex data
+%       metric: The metric used to quantify diversity.
 %       cell_lines: cell array or .grp of lines for which to compute
-%                   pairwise diversities
+%                   pairwise diversities. Default: all in ds.
 %       make_plot: Boolean, whether to make plots. Default 0
 %       show_plot: Boolean, whether to make plot visible. Default 0
 %       save_plot: Boolean, whether to save plots to disk. Default 0
-%       plot_dir: Directory to save plots to. 
-%                 Default
-%                 /cmap/projects/cell_line_diversity/analysis/pdi/fig
+%       plot_dir:  Directory to save plots to. 
+%                
 %
 %Output: pdi: a structure of Pairwise Diversity Index. pdi.mat is a
 %cell_lines x cell_lines matrix with pdi.mat(i,j) being the diversity index
 %between cell_line i and cell_line j
 
 params = {'cell_lines',...
+          'metric',...
           'make_plot',...
           'show_plot',...
           'save_plot',...
           'plot_dir'};
-dflts = {'/cmap/projects/cell_line_diversity/data/phase1_core_lines.grp',...
+dflts = {'',...
+         'basic_wtd_corr',...
          false,...
          false,...
          false,...
-         '/cmap/projects/cell_line_diversity/analysis/pairwise_diversity_index/fig'};
+         ''};
 args = parse_args(params,dflts,varargin{:});
 
-if iscell(args.cell_lines)
+if isempty(args.cell_lines)
+    lines = unique(ds.cdesc(:,ds.cdict('cell_id')));
+elseif iscell(args.cell_lines)
     lines = args.cell_lines;
 else
     lines = parse_grp(args.cell_lines);
 end
+
+%Make sure we have at least two cell lines
+assert(numel(lines) >= 2, 'Must have at least two cell lines')
 
 %initialize pdi
 mat = zeros(numel(lines));
@@ -48,17 +57,12 @@ for ii = 1:numel(lines)
         ds2 = ds_slice(ds,'cidx',find(idx2));
         
         pdi.mat(ii,jj) = compute_diversity_index_two(ds1,ds2,...
+            'metric',args.metric,...
             'make_plot',args.make_plot,...
-            'show_plot',args.show_plot);
+            'show_plot',args.show_plot,...
+            'save_plot',args.save_plot,...
+            'plot_dir',args.plot_dir);
     end
-end
-
-%Save plots to disk
-if args.save_plot
-    savefigures('out',args.plot_dir,...
-                'closefig',true,...
-                'overwrite',true,...
-                'mkdir',false)
 end
 
 %Fill in the lower triangle
