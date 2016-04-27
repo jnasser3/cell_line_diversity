2b0da12e2b09cec33070bfd183ea17c00646e413
Joseph Nasser <jnasser@broadinstitute.org>
2016-04-27 16:16:38 -0400

    Adding support for running ap with exact specification of number of exemplars through bisection search

--- code/alg/run_apcluster.m
+++ code/alg/run_apcluster.m
@@ -13,8 +13,13 @@ function [exemplars,membership] = run_apcluster(ds,varargin)
 %                consideration. Default all in ds.
 %       exclude: a cell array or .grp of objects to exclude from consideration. Default ''
 %       known: a cell array or .grp of known exemplars to cluster around. Default ''
+%       num_exemplar: A positive integer specifying the exact number of
+%           exemplars to return (including known exemplars). Set equal to 0
+%           to just use preferences and let affinity propogation choose
+%           optimal number. Default 0. If not 0, should always be greater
+%           than number of elements in known.
 %       preference: a cell array or .grp of exemplars we want to give preference to.
-%       pref_quantile: A number in [0,1]. Controls the preferences. Lower
+%       pref_quantile: A number typically in [0,1]. Controls the preferences. Lower
 %               means more exemplars. Eg: 0 means all points are exemplars. 
 %               1 means only a single exemplar is chosen. Default .01
 %       pref_factor: Preference factor weight. A higher number means
@@ -29,6 +34,7 @@ params = {'exclude',...
           'include',...
           'known',...
           'preference',...
+          'num_exemplar',...
           'pref_quantile',...
           'pref_factor',...
           'verbose'};
@@ -36,6 +42,7 @@ dflts = {'',...
     '',...
     '',...
     '',...
+    0,...
     .01,...
     1.5,...
     true};
@@ -55,19 +62,75 @@ include = get_array_input(args.include,ds.cid);
 include = intersect(include,ds.cid);
 ds = ds_slice(ds,'cid',include,'rid',include,'ingore_missing',true);
 
-%Set preferences vector
+%Get array variables
 known = get_array_input(args.known,'');
 preference = get_array_input(args.preference,'');
-initial_preference_weight = mk_known_pref(-1*ds.mat,...
-    ds.rid,...
-    known,...
-    preference,...
-    args.pref_quantile,...
-    args.pref_factor);
 
-%Run the clustering algo. Note that apcluster requires a similarity matrix,
-%so change the sign on the distance matrix
-[idx, ~, ~, ~] = apcluster(-1*ds.mat,initial_preference_weight);
+%Make sure num_exemplar is valid.
+if args.num_exemplar
+    assert(isint(args.num_exemplar),'Number of exemplars must be a positive integer')
+    assert(args.num_exemplar > 0,'Number of exemplars must be a positive integer')
+    assert(args.num_exemplar >= numel(unique(known)), 'Number of exemplars should be greater than known!')
+end
+
+%Run ap using a one-shot approach or bisection
+pref_quantile = args.pref_quantile;
+if ~args.num_exemplar
+    %%One Shot
+    %Set preferences vector
+    preference_vector = mk_preference_vector(-1*ds.mat,...
+        ds.rid,...
+        known,...
+        preference,...
+        pref_quantile,...
+        args.pref_factor);
+
+    %Run the clustering algo. Note that apcluster requires a similarity matrix,
+    %so change the sign on the distance matrix
+    [idx, ~, ~, ~] = apcluster(-1*ds.mat,preference_vector);
+else
+    %%Bisection
+    %Set preferences vector
+    idx = 0;
+    low_quantile = 0;
+    high_quantile = .1;
+    K = args.num_exemplar;
+        
+    if args.verbose
+        fprintf('Running Bisection to target %d exemplars \n',K);
+    end
+    
+    %Run a bisection on the preference quantile to search for the exact
+    %number of exemplars
+    while numel(unique(idx)) ~= K
+        
+        temp_quantile = (high_quantile + low_quantile)/2;
+        
+        if args.verbose
+            fprintf('Trying a quantile of %.5f \n',temp_quantile);
+        end
+        
+        preference_vector = mk_preference_vector(-1*ds.mat,...
+            ds.rid,...
+            known,...
+            preference,...
+            temp_quantile,...
+            args.pref_factor);
+        
+        [idx, ~, ~, ~] = apcluster(-1*ds.mat,preference_vector);
+        
+        if args.verbose
+            fprintf('Got %d exemplars\n',numel(unique(idx)));
+        end
+        
+        if numel(unique(idx)) > K
+            low_quantile = temp_quantile;
+        elseif numel(unique(idx)) < K
+            high_quantile = temp_quantile;
+        end
+    end
+        
+end
 
 exemplar_idx = unique(idx);
 exemplars = ds.rid(exemplar_idx);
@@ -75,24 +138,28 @@ membership = ds.rid(idx);
 
 end
 
-function pref = mk_known_pref(sim,objects,known,candidate,t,pref_factor)
-%Given a subset of the objects which are known landmarks, returns a weight
-%vector promoting the inclusion of these landmarks. %
+function pref_vector = mk_preference_vector(sim,objects,known,preferred,t,pref_factor)
+%Create a vector specifying the initial preferance each object has for
+%being an exemplar.
 %
-%Known objects objects get a 'preference' of 1, unkown objects get a
-%preference controlling the total number of exemplars apcluster chooses.
+%The higher the preference, the more likely it is to be chosen as an
+%exemplar. We currently support three levels of hierarchy: known, preferred
+%and everything else. Known objects get an (essentially) infinite
+%preference. Preferred objects get a preference that is pref_factor better
+%than everything else
 n = numel(objects);
 
+%Get preference for 'everything else'
 [pref_min, pref_max] = preferenceRange(sim);
 const = t*pref_min + (1-t)*pref_max;
 
-pref = pref_factor*const*ones(n,1);
+pref_vector = pref_factor*const*ones(n,1);
 
-candidate_idx = ismember(objects,candidate);
-pref(candidate_idx) = const;
+candidate_idx = ismember(objects,preferred);
+pref_vector(candidate_idx) = const;
 
 known_idx = ismember(objects,known);
-pref(known_idx) = -1*const*100;
+pref_vector(known_idx) = -1*const*100;
 
 
 end
