c7e8f860a52032ba0c49c6ffaa98d311eea3f2bc
Joseph Nasser <jnasser@broadinstitute.org>
2016-05-04 12:52:55 -0400

    Update to return test statistics and background statistics

--- code/util/evaluate_pmedian_loss.m
+++ code/util/evaluate_pmedian_loss.m
@@ -1,20 +1,23 @@
-function loss = evaluate_pmedian_loss(ds,exemplar)
-%loss = evaluate_pmedian_loss(ds,exemplars)
+function objective = evaluate_pmedian_loss(ds,exemplar,type)
+%objective = evaluate_pmedian_loss(ds,exemplars)
 %
-%Calculate the loss function for a pairwise distance matrix and a set of
-%exemplars. The loss is equal to the sum of the distances from each
-%non-exemplar to it's nearest exemplar.
+%Calculate the objective function for a pairwise distance or similarity matrix and a set of
+%exemplars. If type = 'distance', the objective is the average 
+%distance from each point to its nearest exemplar. If type = 'similarity',
+%the objective is the average similarity from each point to its
+%nearest exemplar.
 
 exemplar_idx = ismember(ds.rid,exemplar);
-loss = sum(min(ds.mat(:,exemplar_idx),[],2));
 
-%Average loss for easier interpretation
-loss = loss / numel(ds.rid);
+switch type
+    case 'distance'
+        objective = sum(min(ds.mat(:,exemplar_idx),[],2));
+    case 'similarity'
+        objective = sum(max(ds.mat(:,exemplar_idx),[],2));
+end
 
-% %Slow version of the vectorized code
-% for ii = 1:numel(ds.rid);
-%     loss = loss + min(ds.mat(ii,exemplar_idx));
-% end
+%Average objective for easier interpretation
+objective = objective / numel(ds.rid);
 
 
 end
--- code/util/evaluate_total_diversity.m
+++ code/util/evaluate_total_diversity.m
@@ -1,5 +1,5 @@
-function evaluate_total_diversity(ds, lines, varargin )
-% evaluate_total_diversity(ds, lines, varargin )
+function [test_statistic, bkg] = evaluate_total_diversity(ds, lines, varargin)
+% test_statistic = evaluate_total_diversity(ds, lines, varargin)
 %
 % Start with a universe of objects and pairwise distances between them.
 % Given a subset of these objects, computes the sum of their mutual 
@@ -7,9 +7,15 @@ function evaluate_total_diversity(ds, lines, varargin )
 %
 % Input
 %       ds: A square struct of pairwise distances
-%       include: The set of lines to include in the analysis
 %       lines: An array of cell arrays. Each array contains a set of lines to evaluate.
 %           Each array must have the same number of elements.
+%           Note: If only a single array is present it should be passed in
+%           as an array containing a single array. Ie
+%           evaluate_total_diversity(ds, {lines_array})
+%       ds_type: String. Either 'similarity' or 'distance', denoting
+%           ds contains either similarities or distances. Default
+%           'similarity'
+%       include: The set of lines to include in the analysis
 %       lines_labels: An array of labels for lines for plotting purposes.
 %       objective: The objective function to evaluate. Options are divr
 %           (sum of distances among chosen lines) or repr (sum of minimum
@@ -19,25 +25,41 @@ function evaluate_total_diversity(ds, lines, varargin )
 %       fixed_lines: A subset of objects to include in the calculation but
 %                   not to evaluate
 %       nperms: Number of permutations to compare against. Default 1000
+%       make_plot: Boolean. Default true
+%
+% Output
+%       test_statistic: array of objective loss for each array in lines
+%       bkg: Objective loss under random selection of exemplars.
 
 params = {'include',...
     'fixed_lines',...
     'objective',...
     'exclude',...
     'lines_labels',...
-    'nperms'};
+    'ds_type',...
+    'nperms',...
+    'make_plot'};
 dflts = {'',...
     '',...
     'repr',...
     '',...
     '',...
-    1000};
+    'similarity',...
+    1000,...
+    true};
 args = parse_args(params,dflts,varargin{:});
+
+%Check ds_type
+assert(ismember(args.ds_type,{'distance','similarity'}),'Invalid ds_type entry')
 
 %Make sure all arrays in lines have the same number of elements.
 sizes = cellfun('length',lines);
 assert(range(sizes) == 0, 'All arrays must have the same number of elements');
-assert(length(lines) == length(args.lines_labels), 'Must have same number of lines and labels')
+
+%only need to check this if we are making plots.
+if args.make_plot
+    assert(length(lines) == length(args.lines_labels), 'Must have same number of lines and labels')
+end
 
 %Subset ds if needed
 exclude = get_array_input(args.exclude,'');
@@ -63,7 +85,7 @@ for ii = 1:length(lines)
         case 'divr'
                 test_statistic(ii) = compute_total_diversity(ds,[this_lines; fixed_lines]);
         case 'repr'
-                test_statistic(ii) = evaluate_pmedian_loss(ds, [this_lines; fixed_lines]);
+                test_statistic(ii) = evaluate_pmedian_loss(ds, [this_lines; fixed_lines], args.ds_type);
     end
 end
 
@@ -79,33 +101,36 @@ for ii = 1:args.nperms
         case 'divr'
             bkg(ii) = compute_total_diversity(ds,[this_lines; fixed_lines]);  
         case 'repr'
-            bkg(ii) = evaluate_pmedian_loss(ds, [this_lines; fixed_lines]);
+            bkg(ii) = evaluate_pmedian_loss(ds, [this_lines; fixed_lines],  args.ds_type);
     end
 end
 
 %plot
-figure;
-histogram(bkg,'DisplayName','Background');
-hold on
-g = gca;
-for ii = 1:length(lines)
-    plot([test_statistic(ii) test_statistic(ii)], g.YLim,...
-        'DisplayName', args.lines_labels{ii},...
-        'LineWidth',2)
+if args.make_plot
+    figure;
+    histogram(bkg,'DisplayName','Background');
     hold on
+    g = gca;
+    for ii = 1:length(lines)
+        plot([test_statistic(ii) test_statistic(ii)], g.YLim,...
+            'DisplayName', args.lines_labels{ii},...
+            'LineWidth',2)
+        hold on
+    end
+    xlabel('Objective')
+    ylabel('Frequency')
+    title_str = sprintf(['Objective with respect to random background\n',...
+        'Pairwise Distance Dataset: %s\n',...
+        'Num Exemplar: %d\n',...
+        'Objective type is: %s\n',...
+        'nperms = %d'],...
+        inputname(1),numel([lines{1}; fixed_lines]),args.objective,args.nperms);
+    title(title_str,'Interpreter','none')
+    grid on
+    legend('show')
+    leg = legend;
+    set(leg, 'Interpreter', 'None')
 end
-xlabel('Objective')
-ylabel('Frequency')
-title_str = sprintf(['Objective with respect to random background\n',...
-    'Num Exemplar: %d\n',...
-    'Objective type is: %s\n',...
-    'nperms = %d\n'],...
-    numel([lines{1}; fixed_lines]),args.objective,args.nperms);
-title(title_str,'Interpreter','none')
-grid on
-legend('show')
-leg = legend;
-set(leg, 'Interpreter', 'None')
 
 end
 
